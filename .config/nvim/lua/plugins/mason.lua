5f1c72206da013efc76b65f2cdb9fb1e9546618e
Meredith Howard <mhoward@roomag.org>
2025-12-30 22:22:51 -0600

    Back to treesitter main, the wasm thing wont happen soon

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,26 @@
 ---@type LazySpec
 return {
   -- mason is nonlazy so my executable tests work
-  { "mason-org/mason.nvim", config = true },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tree-sitter-cli",
+        "lua-language-server",
+      }
+    },
+    config = function(_, opts)
+      local reg = require("mason-registry")
+
+      require("mason").setup({})
+
+      for _, pkg in ipairs(opts.ensure_installed) do
+        if not reg.is_installed(pkg) then
+          reg.get_package(pkg):install()
+        end
+      end
+
+    end
+  },
 }
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,17 +2,16 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
+    lazy = false,
     dependencies = {
+      -- tree-sitter-cli via mason.nvim
+      "mason-org/mason.nvim",
       "RRethy/nvim-treesitter-endwise",
     },
     opts = {
-      highlight = {
-        enable = true,
-        disable = { "perl" },
-      },
-      indent = { enable = true },
-      auto_install = true,
+      highlight = { disable = { "perl" } },
       ensure_installed = {
         "c",
         "comment",
@@ -32,25 +31,43 @@ return {
         "vim",
         "vimdoc",
       },
-      parser_install_dir = vim.fn.stdpath("data") .. "/site",
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter").install(opts.ensure_installed)
 
       vim.api.nvim_set_hl(0, "@keyword.pod", { link = "MoreMsg" })
       vim.api.nvim_set_hl(0, "@markup.raw.pod", { link = "PreProc" })
 
+      local function activate(buf, language)
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+        if not vim.tbl_contains(opts.highlight.disable, language) then
+          vim.treesitter.start(buf, language)
+        end
+      end
+
+      local function try_install(buf, language)
+        require("nvim-treesitter").install({language}):await(function()
+          if vim.treesitter.language.add(language) then
+            activate(buf, language)
+          end
+        end)
+      end
+
       vim.api.nvim_create_autocmd("FileType", {
         group = vim.api.nvim_create_augroup("treesitter.setup", { clear = true }),
         callback = function(args)
-          local filetype = args.match
+          local buf, filetype = args.buf, args.match
+
           local language = vim.treesitter.language.get_lang(filetype) or filetype
           if not vim.treesitter.language.add(language) then
+            try_install(buf, language)
             return
           end
 
-          vim.wo.foldmethod = "expr"
-          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          activate(buf, language)
         end,
       })
     end,
