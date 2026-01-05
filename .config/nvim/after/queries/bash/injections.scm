be976475cee278b73e5f3fc4fcca2f4873872ea8
Meredith Howard <mhoward@roomag.org>
2026-01-05 11:19:11 -0600

    downcase handler in flux but it seems unnecessary?

--- .config/nvim/after/queries/bash/injections.scm
+++ .config/nvim/after/queries/bash/injections.scm
@@ -3,5 +3,4 @@
 ((heredoc_redirect
   (heredoc_body) @injection.content
   (heredoc_end) @injection.language)
-  (#gsub! @injection.language "^END_" "")
-  (#downcase! @injection.language))
+  (#gsub! @injection.language "^END_" ""))
--- .config/nvim/after/queries/ruby/injections.scm
+++ .config/nvim/after/queries/ruby/injections.scm
@@ -3,5 +3,4 @@
 (heredoc_body
   (heredoc_content) @injection.content
   (heredoc_end) @injection.language
-  (#gsub! @injection.language "^END_" "")
-  (#downcase! @injection.language))
+  (#gsub! @injection.language "^END_" ""))
