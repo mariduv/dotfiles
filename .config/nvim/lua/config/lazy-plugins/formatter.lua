75d821efed68ce10c9234a5144b8a66c27b7836f
Meredith Howard <mhoward@roomag.org>
2026-06-18 12:12:05 -0500

    tweak conform.nvim more

--- .config/nvim/lua/config/lazy-plugins/formatter.lua
+++ .config/nvim/lua/config/lazy-plugins/formatter.lua
@@ -4,22 +4,25 @@ return {
     "stevearc/conform.nvim",
     event = "VeryLazy",
     opts = {
+      default_format_opts = {
+        lsp_format = "prefer",
+      },
       formatters_by_ft = {
-        mail = { "pandoc_markdown" },
-        markdown = { "pandoc_markdown" },
+        dockerfile = { "dockerfmt" },
+        elixir = { "mix" },
+        go = { "goimports", "gofumpt" },
+        lua = { "stylua" },
+        mail = { "pandoc_markdown", "injected" },
+        markdown = { "pandoc_markdown", "injected" },
         perl = { "perltidy" },
         ruby = { "standardrb", "rubocop", stop_after_first = true },
         sh = { "shfmt" },
         sql = { "pg_format" },
-        -- LSP-handled:
-        -- elixir = { "mix" },
-        -- go = { "goimports", "gofumpt" },
-        -- lua = { "stylua" },
       },
       formatters = {
         pandoc_markdown = {
           command = "pandoc",
-          args = { "-f", "markdown", "-t", "markdown" },
+          args = { "-f", "markdown", "-t", "markdown", "--wrap=preserve"},
           stdin = true,
         },
         shfmt = {
@@ -41,8 +44,7 @@ return {
         end
 
         return {
-          timeout_ms = 500,
-          lsp_format = "prefer",
+          timeout_ms = 1000,
         }
       end
     },
